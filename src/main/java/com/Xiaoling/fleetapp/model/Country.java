6b56229de994c51ca7b8c6fbe1cbd17f3c16b9cc
qixiaoling <x.qi@novi-education.nl>
2021-01-25 18:23:07 +0100

    solved

--- src/main/java/com/Xiaoling/fleetapp/model/Country.java
+++ src/main/java/com/Xiaoling/fleetapp/model/Country.java
@@ -12,9 +12,6 @@ import java.io.Serializable;
 import java.util.*;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class)
 public class Country {
     @Id
@@ -29,5 +26,73 @@ public class Country {
     @OneToMany(mappedBy = "country")
     private List<State> states;
 
+    public Country() {
+    }
+
+    public Country(Integer id, String code, String capital, String description, String nationality, String continent, List<State> states) {
+        this.id = id;
+        this.code = code;
+        this.capital = capital;
+        this.description = description;
+        this.nationality = nationality;
+        this.continent = continent;
+        this.states = states;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public List<State> getStates() {
+        return states;
+    }
+
+    public void setStates(List<State> states) {
+        this.states = states;
+    }
 }
 
